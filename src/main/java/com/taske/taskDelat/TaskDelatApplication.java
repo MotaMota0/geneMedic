392a5cb69004a60602105c14a77fa4c12bb1e5c9
Arman <aidarbekovarman68@gmail.com>
2025-12-08 22:50:25 +0500

    last chance

--- src/main/java/com/taske/taskDelat/TaskDelatApplication.java
+++ src/main/java/com/taske/taskDelat/TaskDelatApplication.java
@@ -3,9 +3,14 @@ package com.taske.taskDelat;
 import jakarta.persistence.Cacheable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class
+})
 @EnableCaching
 public class TaskDelatApplication {
 
---
+++ src/main/java/com/taske/taskDelat/configuration/DatabaseConfig.java
@@ -0,0 +1,51 @@
+package com.taske.taskDelat.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Value("${SPRING_DATASOURCE_URL}")
+    private String url;
+
+    @Value("${SPRING_DATASOURCE_USERNAME}")
+    private String username;
+
+    @Value("${SPRING_DATASOURCE_PASSWORD}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(url.replace("postgres://", "jdbc:postgresql://")); // фикс Railway URL
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource());
+        em.setPackagesToScan("com.taske.taskDelat"); // твои entity
+
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        properties.setProperty("hibernate.hbm2ddl.auto", "validate");
+        em.setJpaProperties(properties);
+
+        return em;
+    }
+}
